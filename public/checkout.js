f4d653c5e75f15f5d483a5bb402a16a6383943f8
Giuseppe Verni <gverni@stripe.com>
2023-11-14 16:40:29 +0000

    Frontend Bug fix

--- public/checkout.js
+++ public/checkout.js
@@ -1,20 +1,24 @@
 // Create a Checkout Session as soon as the page loads
-async function initialize() {
-  const response = await fetch("/.netlify/functions/create-checkout-session", {
-    method: "POST",
-  });
-
-  const { clientSecret } = await response.json();
-
-  const checkout = await stripe.initEmbeddedCheckout({
-    clientSecret,
-  });
-
-  // Mount Embedded Checkout
-  checkout.mount("#checkout");
-}
 
 document.addEventListener("DOMContentLoaded", async () => {
+  async function initialize() {
+    const response = await fetch(
+      "/.netlify/functions/create-checkout-session",
+      {
+        method: "POST",
+      }
+    );
+
+    const { clientSecret } = await response.json();
+
+    const checkout = await stripe.initEmbeddedCheckout({
+      clientSecret,
+    });
+
+    // Mount Embedded Checkout
+    checkout.mount("#checkout");
+  }
+
   const { publishableKey } = await fetch("/.netlify/functions/config").then(
     (r) => r.json()
   );
@@ -25,5 +29,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   const stripe = Stripe(publishableKey, {
     apiVersion: "2020-08-27",
   });
+
   initialize();
 });
