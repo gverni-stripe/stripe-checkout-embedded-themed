5f4ef90de5f02979bbe0eafb61bae97e80ddc494
Giuseppe Verni <gverni@stripe.com>
2023-11-14 16:15:30 +0000

    Change functions url

--- public/checkout.js
+++ public/checkout.js
@@ -5,7 +5,7 @@ initialize();
 
 // Create a Checkout Session as soon as the page loads
 async function initialize() {
-  const response = await fetch("/create-checkout-session", {
+  const response = await fetch("/.netlify/functions/create-checkout-session", {
     method: "POST",
   });
 
