13a7b3ec9a8c8951334a73a5fe690b3e192760b4
Giuseppe Verni <gverni@stripe.com>
2023-11-15 20:48:02 +0000

    Make index a SPA

---
+++ public/index.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  const { publishableKey } = await fetch("/.netlify/functions/config").then(
+    (r) => r.json()
+  );
+  if (!publishableKey) {
+    alert("Please set your Stripe publishable API key in the .env file");
+  }
+
+  const stripe = Stripe(publishableKey, {
+    apiVersion: "2020-08-27",
+  });
+
+  document.getElementById("get-started").onclick = async function () {
+    const response = await fetch(
+      "/.netlify/functions/create-checkout-session",
+      {
+        method: "POST",
+      }
+    );
+
+    const { clientSecret } = await response.json();
+
+    const checkout = await stripe.initEmbeddedCheckout({
+      clientSecret,
+    });
+
+    // Hide Pricing Header & Pricing 
+    document.getElementByClassName('pricing-header').style.display = 'none';
+    document.getElementByClassName('pricing-plans').style.display = 'none';
+
+    // Mount Embedded Checkout
+    checkout.mount("#checkout");
+  };
+});
