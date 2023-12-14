469e7f5fa947f6e8a18b9d5c1efc39496aa3a8a8
Giuseppe Verni <gverni@stripe.com>
2023-12-14 17:12:25 +0000

    Change to setup mode

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,29 +1,17 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
   // apiVersion: '2020-08-27',
   appInfo: {
-    name: "stripe-checkout-embedded-themed",
+    name: "stripe-checkout-embedded-themed-setup",
     url: "https://github.com/gverni-stripe/stripe-checkout-embedded-themed"
   }
 });
 
-const lineItems = [
-  {
-    price_data: {
-      unit_amount: 1500,
-      currency: 'gbp',
-      product_data: {
-        name: 'Pro Plan - First Month',
-      },
-    },
-    quantity: 1,
-  }
-]
-
 exports.handler = async () => {
   const session = await stripe.checkout.sessions.create({
     ui_mode: "embedded",
-    line_items: lineItems,
-    mode: "payment",
+    mode: "setup",
+    customer_creation: "always",
+    currency: "gbp",
     return_url: `${process.env.URL}/return.html?session_id={CHECKOUT_SESSION_ID}`,
   });
 
